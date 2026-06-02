d029f321b8a25e04ec9dd09cb280c1670ad9ebd6
Alec Burnworth <alec.burnworth@gmail.com>
2026-06-01 19:55:31 -0500

    committing actions.ts fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,10 +61,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
-        // If a database error occurs, return a more specific error.
-        return {
-        message: 'Database Error: Failed to Create Invoice.',
-        };
+        console.error(error);
+        throw new Error('Database Error: Failed to create invoice.');
     }
 
     // Revalidate the cache for the invoices page and redirect the user.
@@ -87,11 +85,9 @@ export async function updateInvoice(id: string, formData: FormData) {
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-    } catch(error){
+    } catch (error) {
         console.error(error);
-        return {
-            message: 'Database Error: Failed to update invoice.',
-        };
+        throw new Error('Database Error: Failed to update invoice.');
     }
    
     revalidatePath('/dashboard/invoices');
